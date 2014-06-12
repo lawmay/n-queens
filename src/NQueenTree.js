26345a8690678b1efe28a6782e8daac30bc49921
lawmay <lmay01@gmail.com>
2014-06-12 16:05:41 -0700

    added NQueenTree and changed shift to pop for optimization

--- src/NQueenTree.js
+++ src/NQueenTree.js
@@ -1,8 +1,8 @@
-var NRookTree = function(n) {
+var NQueenTree = function(n) {
   this.solutions = [];
   this.n = n;
   this.nodeQueue = [];
-  this.topNode = new Node(n, null, null, []);
+  this.topNode = new Node(n, null, null);
   if (n === 1) {
     this.solutionsCount = 1;
   } else {
@@ -12,7 +12,7 @@ var NRookTree = function(n) {
   // this.buildTree();
 };
 
-NRookTree.prototype.buildTree = function() {
+NQueenTree.prototype.buildTree = function() {
   // fill out the first row with nodes in every square
   this.addParentChildren();
 
@@ -20,8 +20,8 @@ NRookTree.prototype.buildTree = function() {
   // when we shift a node out of the nodeQueue, we add all of it's children
   //   into the queue
   while (this.nodeQueue.length) {
-    // pop one node out from nodeQueue
-    var currentNode = this.nodeQueue.pop(); // changed from shift to pop, big O change of linear to constant
+    // shift one node out from nodeQueue
+    var currentNode = this.nodeQueue.shift();
     // collect that node's children
     var currentChildren = currentNode.addChildren();
     // increment solutionsCount if a node is added and is on the last row
@@ -35,15 +35,15 @@ NRookTree.prototype.buildTree = function() {
   }
 };
 
-NRookTree.prototype.addParentChildren = function() {
+NQueenTree.prototype.addParentChildren = function() {
   for (var i = 0; i < this.n; i++) {
-    var newNode = new Node(this.n, 0, i, []);
+    var newNode = new Node(this.n, 0, i);
     this.nodeQueue.push(newNode);
     this.topNode.children.push(newNode);
   }
 };
 
-NRookTree.prototype.dps = function() {
+NQueenTree.prototype.dps = function() {
   var nodes = [this.topNode];
   var nodePoss = [];
 
@@ -57,40 +57,43 @@ NRookTree.prototype.dps = function() {
   return nodePoss;
 };
 
-var Node = function(n, row, column, prevCols) {
+var Node = function(n, row, column, validCols) {
   this.n = n;
   this.row = row;
   this.col = column;
   this._parent;
   this.children = [];
-  this.previousCols = prevCols || {};
+  // this.addParentChildren();
+  this.validCols = validCols || [];
+  if (!validCols) {
+    for (var i = 0; i < n; i++) {
+      this.validCols.push(i);
+    }
+  }
+  this.nodes = [];
 };
 
 Node.prototype.addChildren = function() {
   // don't add any children if you are on the edge of the board
-  if (this.row === (this.n - 1)) {
+  if (this.row === (this.n-1)) {
     return [];
   }
 
-  // iterate through the next row, if the column hasn't been used before, it's a valid move
-  for (var column = 0; column < this.n; column++) {
-    // it's a valid move...
-    // if the column hasn't been used in a previous move or on this current move
-    if (this.previousCols.indexOf(column) === -1 && column !== this.col) {
-      var newPrevCols = this.previousCols.concat(this.col);
-      var newChild = new Node(this.n, this.row + 1, column, newPrevCols);
-      newChild._parent = this;
-      this.children.push(newChild);
-    }
+  this.validCols.splice(this.validCols.indexOf(this.col), 1);
+  for (var i = 0; i < this.validCols.length; i++) {
+    var newChild = new Node(this.n, this.row + 1, this.validCols[i], this.validCols.slice());
+    newChild._parent = this;
+    this.children.push(newChild);
   }
+
   return this.children;
 };
 
-NRookTree.prototype.findASolution = function() {
+NQueenTree.prototype.findASolution = function() {
   // make storage array
   var storage = [];
   // place node on first row
-  var newNode = new Node(this.n, 0, 0, []);
+  var newNode = new Node(this.n, 0, 0);
   // keep adding 1 child to node
   while (newNode !== undefined) {
     // push newNode's position into storage array
@@ -100,13 +103,3 @@ NRookTree.prototype.findASolution = function() {
   // return storage array
   return storage;
 };
-
-
-
-
-
-
-
-
-
-
